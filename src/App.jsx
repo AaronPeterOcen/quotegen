57e3263b61fb5366be5ef727acf3496857be3f13
aaronPeter <aaronpeter@pop-os.localdomain>
2023-04-12 10:21:40 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import React from "react";
+import "./App.css";
+import { Quotes } from "./components/Quotes";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <Quotes />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Quotes.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+export const Quotes = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  return <div>Quotes</div>;
+};
